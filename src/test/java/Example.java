9fa1f404662201c57fe2bb21da7e86cedbe47480
Daniel Flower <git@danielflower.com>
2019-04-04 22:11:41 +0800

    Fixed the example reverse proxy

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -9,18 +9,23 @@ import static io.muserver.MuServerBuilder.muServer;
 public class Example {
 
     public static void main(String[] args) {
+
+        // This creates a proxy of the MDN docs
+
         MuServer server = muServer()
-            .withHttpPort(12000)
+            .withHttpPort(12080)
+            .withHttpsPort(12443)
             .addHandler(
                 ReverseProxyBuilder.reverseProxy()
                     .withUriMapper(request -> {
                         String pathAndQuery = Murp.pathAndQuery(request.uri());
                         return URI.create("https://developer.mozilla.org").resolve(pathAndQuery);
                     })
+                    .proxyHostHeader(false)
             )
             .start();
 
-        System.out.println("Load " + server.uri() + " to proxy to the target");
+        System.out.println("Load " + server.httpUri() + " or " + server.httpsUri() + " to proxy to the target");
 
     }
 }
